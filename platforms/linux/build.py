9937dc522937e9d9a57f73bf1cbf3aa1bd04ca08
Stefan Pfeifer <s-pfeifer@gmx.net>
2017-07-03 00:02:31 +0200

    First attempt at building an rpm package

--- platforms/linux/build.py
+++ platforms/linux/build.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 import shutil
+import tarfile
 
 def build_vtk(build_dir, output_dir):
     subprocess.call(["apt", "install", "-y", "qtbase5-dev", "libqt5x11extras5-dev", "libxt-dev"])
@@ -49,7 +50,7 @@ def build_install_files(output_dir):
                   "Comment=Bow and arrow physics simulation\n"
                   "Categories=Education;Science;NumericalAnalysis;Physics;Engineering;\n"
                   "MimeType=application/json\n")
-    desktop.close()
+    desktop.close()    # Todo: Use 'with' stament
 
 def build_deb_package(version, build_dir, output_dir):
     # Create directory structure that will be installed
@@ -65,14 +66,70 @@ def build_deb_package(version, build_dir, output_dir):
                   "Homepage: https://bow-simulator.gitlab.io\n"
                   "Description: Bow and arrow physics simulation\n"
                   "Depends: qt5-default\n")
-    control.close()
+    control.close()    # Todo: Use 'with' stament
     
     # Build package
     subprocess.call(["dpkg-deb", "--build", build_dir, output_dir + "/bow-simulator-" + version + "-" + "amd64" + ".deb"])
 
 def build_rpm_package(version, build_dir, output_dir):
-    print("BUILD RPM PACKAGE")
+    # Create directory structure
+    os.makedirs(build_dir + "/BUILD")
+    os.makedirs(build_dir + "/RPMS")
+    os.makedirs(build_dir + "/SOURCES")
+    os.makedirs(build_dir + "/SPECS")
+    os.makedirs(build_dir + "/SRPMS")
+    os.makedirs(build_dir + "/tmp")
+
+    # Create .rpmmacros file
+    rpmmacros = open(build_dir + "/.rpmmacros", "w")
+    rpmmacros.write("%_topdir " + build_dir + "\n"
+                    "%_tmppath " + build_dir + "/tmp\n")
+    rpmmacros.close()    # Todo: Use 'with' stament
+
+    # Create .tar file of project
+    files_dir = build_dir + "/SOURCES/bow-simulator"
+    build_install_files(files_dir)
+    tar = tarfile.open(build_dir + "/SOURCES/bow-simulator.tar.gz", "w:gz")
+    tar.add(files_dir, arcname=os.path.basename(files_dir))
+    tar.close()    # Todo: Use 'with' stament
+
+    # Create .spec file
+    spec = open(build_dir + "/SPECS/bow-simulator.spec", "w")
+    spec.write("%define __spec_install_post %{nil}\n"
+               "%define   debug_package %{nil}\n"
+               "%define __os_install_post %{_dbpath}/brp-compress\n"
+               "\n"
+               "Summary: Bow and arrow physics simulation\n"
+               "Name: bow-simulator\n"
+               "Version: " + version + "\n"
+               "Release: 0\n"                  # Todo
+               "License: GPL3\n"
+               "Packager: Stefan Pfeifer <s-pfeifer@gmx.net>\n"
+               "Group: Development/Tools\n"    # Todo
+               "SOURCE0: bow-simulator.tar.gz\n"
+               "URL: https://bow-simulator.gitlab.io\n"
+               "\n"
+               "BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root\n"
+               "\n"
+               "%description\n"
+               "%{summary}\n"
+               "\n"
+               "%prep\n"
+               "%setup -q\n"
+               "\n"
+               "%build\n"
+               "# Empty section\n"
+               "\n"
+               "%install\n"
+               "rm -rf %{buildroot}\n"
+               "mkdir -p %{buildroot}\n"
+               "cp -a * %{buildroot}\n")
+    spec.close()
+
+    # Build package
+    print("SPECFILE: " + build_dir + "/SPECS/bow-simulator.spec")
+    subprocess.call(["rpmbuild", "-ba", build_dir + "/SPECS/bow-simulator.spec"])
 
 def build_packages(version, build_dir, output_dir):
-    build_deb_package(version, build_dir + "/build-deb", output_dir)
+    # build_deb_package(version, build_dir + "/build-deb", output_dir)
     build_rpm_package(version, build_dir + "/build-rpm", output_dir)
