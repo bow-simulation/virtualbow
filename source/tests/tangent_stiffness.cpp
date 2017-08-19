55cc2ad865b5d494d82d0579f04f9cf72c559f3a
Stefan Pfeifer <s-pfeifer@gmx.net>
2017-08-19 21:34:07 +0200

    Include tests in CMakeLists.txt

--- source/tests/tangent_stiffness.cpp
+++ source/tests/tangent_stiffness.cpp
@@ -1,6 +1,6 @@
-#include "../fem/System.hpp"
-#include "../fem/elements/BarElement.hpp"
-#include "../fem/elements/BeamElement.hpp"
+#include "fem/System.hpp"
+#include "fem/elements/BarElement.hpp"
+#include "fem/elements/BeamElement.hpp"
 
 #include <catch.hpp>
 #include <iostream>
