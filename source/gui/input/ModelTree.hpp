063bcb1fc0e6cc9a740537f03c7e6740ab09c942
Stefan Pfeifer <s-pfeifer@gmx.net>
2017-11-25 21:36:24 +0100

    Make input dialogs non-modal

--- source/gui/input/ModelTree.hpp
+++ source/gui/input/ModelTree.hpp
@@ -14,13 +14,11 @@ class TreeItem: public QObject, public QTreeWidgetItem
     Q_OBJECT
 
 public:
-    std::function<void()> action;
-
-    template<class parent_t>
-    TreeItem(parent_t* parent, DocumentNode& doc_node, const QString& name, const QIcon& icon, const std::function<void()>& action = []{})
+    template<class T>
+    TreeItem(T* parent, DocumentNode& doc_node, const QString& name, const QIcon& icon, std::function<QDialog*()> create_dialog)
         : QTreeWidgetItem(parent, {name}),
-          action(action),
-          doc_node(doc_node)
+          doc_node(doc_node),
+          create_dialog(create_dialog)
     {
         this->setIcon(0, icon);
         QObject::connect(&doc_node, &DocumentNode::error_changed, this, &TreeItem::update_error);
@@ -41,8 +39,19 @@ public:
         }
     }
 
+    void show_dialog()
+    {
+        if(dialog == nullptr || !dialog->isVisible())
+            dialog = create_dialog();
+
+        dialog->show();
+        dialog->activateWindow();
+    }
+
 private:
     DocumentNode& doc_node;
+    std::function<QDialog*()> create_dialog;
+    QDialog* dialog = nullptr;
 };
 
 class ModelTree: public QTreeWidget
@@ -50,29 +59,46 @@ class ModelTree: public QTreeWidget
 public:
     ModelTree(InputData& data)
     {
+        new TreeItem(this, data.settings, "Settings", QIcon(":/icons/model-tree/settings"), [&]
+        {
+            auto dialog = new NumberDialog(this, "Settings");
+            dialog->addGroup("General");
+            dialog->addField("Limb elements:", "", data.settings.n_elements_limb);
+            dialog->addField("String elements:", "", data.settings.n_elements_string);
+
+            dialog->addGroup("Statics");
+            dialog->addField("Draw steps:", "", data.settings.n_draw_steps);
+
+            dialog->addGroup("Dynamics");
+            dialog->addField("Time span factor:", "", data.settings.time_span_factor);
+            dialog->addField("Time step factor:", "", data.settings.time_step_factor);
+            dialog->addField("Sampling rate:", "Hz", data.settings.sampling_rate);
+
+            return dialog;
+        });
+
         new TreeItem(this, data.meta.comments, "Comments", QIcon(":/icons/model-tree/comments"), [&]
         {
-            CommentDialog dialog(this, data);
-            dialog.exec();
+            auto dialog = new CommentDialog(this, data);
+            return dialog;
         });
 
         new TreeItem(this, data.settings, "Settings", QIcon(":/icons/model-tree/settings"), [&]
         {
-            NumberDialog dialog(this, "Settings");
+            auto dialog = new NumberDialog(this, "Settings");
+            dialog->addGroup("General");
+            dialog->addField("Limb elements:", "", data.settings.n_elements_limb);
+            dialog->addField("String elements:", "", data.settings.n_elements_string);
 
-            dialog.addGroup("General");
-            dialog.addField("Limb elements:", "", data.settings.n_elements_limb);
-            dialog.addField("String elements:", "", data.settings.n_elements_string);
+            dialog->addGroup("Statics");
+            dialog->addField("Draw steps:", "", data.settings.n_draw_steps);
 
-            dialog.addGroup("Statics");
-            dialog.addField("Draw steps:", "", data.settings.n_draw_steps);
+            dialog->addGroup("Dynamics");
+            dialog->addField("Time span factor:", "", data.settings.time_span_factor);
+            dialog->addField("Time step factor:", "", data.settings.time_step_factor);
+            dialog->addField("Sampling rate:", "Hz", data.settings.sampling_rate);
 
-            dialog.addGroup("Dynamics");
-            dialog.addField("Time span factor:", "", data.settings.time_span_factor);
-            dialog.addField("Time step factor:", "", data.settings.time_step_factor);
-            dialog.addField("Sampling rate:", "Hz", data.settings.sampling_rate);
-
-            dialog.exec();
+            return dialog;
         });
 
         auto item_parameters = new QTreeWidgetItem(this, {"Parameters"});
@@ -80,55 +106,55 @@ public:
 
         new TreeItem(item_parameters, data.profile, "Profile", QIcon(":/icons/model-tree/profile"), [&]
         {
-            ProfileDialog dialog(this, data);
-            dialog.exec();
+            auto dialog = new ProfileDialog(this, data);
+            return dialog;
         });
 
         new TreeItem(item_parameters, data.width, "Width", QIcon(":/icons/model-tree/width"), [&]
         {
-            WidthDialog dialog(this, data);
-            dialog.exec();
+            auto dialog = new WidthDialog(this, data);
+            return dialog;
         });
 
         new TreeItem(item_parameters, data.height, "Height", QIcon(":/icons/model-tree/height"), [&]
         {
-            HeightDialog dialog(this, data);
-            dialog.exec();
+            auto dialog = new HeightDialog(this, data);
+            return dialog;
         });
 
         new TreeItem(item_parameters, data.material, "Material", QIcon(":/icons/model-tree/material"), [&]
         {
-            NumberDialog dialog(this, "Material");
-            dialog.addField("rho:", "kg/m³", data.material.rho);    // Todo: Use unicode character (\u2374). Problem: Windows
-            dialog.addField("E:", "N/m²", data.material.E);
-            dialog.exec();
+            auto dialog = new NumberDialog(this, "Material");
+            dialog->addField("rho:", "kg/m³", data.material.rho);    // Todo: Use unicode character (\u2374). Problem: Windows
+            dialog->addField("E:", "N/m²", data.material.E);
+            return dialog;
         });
 
         new TreeItem(item_parameters, data.string, "String", QIcon(":/icons/model-tree/string"), [&]
         {
-            NumberDialog dialog(this, "String");
-            dialog.addField("Strand stiffness:", "N/100%", data.string.strand_stiffness);
-            dialog.addField("Strand density:", "kg/m", data.string.strand_density);
-            dialog.addField("Number of strands:", "", data.string.n_strands);
-            dialog.exec();
+            auto dialog = new NumberDialog(this, "String");
+            dialog->addField("Strand stiffness:", "N/100%", data.string.strand_stiffness);
+            dialog->addField("Strand density:", "kg/m", data.string.strand_density);
+            dialog->addField("Number of strands:", "", data.string.n_strands);
+            return dialog;
         });
 
         new TreeItem(item_parameters, data.masses, "Masses", QIcon(":/icons/model-tree/masses"), [&]
         {
-            NumberDialog dialog(this, "Masses");
-            dialog.addField("String center:", "kg", data.masses.string_center);
-            dialog.addField("String tip:", "kg", data.masses.string_tip);
-            dialog.addField("Limb tip:", "kg", data.masses.limb_tip);
-            dialog.exec();
+            auto dialog = new NumberDialog(this, "Masses");
+            dialog->addField("String center:", "kg", data.masses.string_center);
+            dialog->addField("String tip:", "kg", data.masses.string_tip);
+            dialog->addField("Limb tip:", "kg", data.masses.limb_tip);
+            return dialog;
         });
 
         new TreeItem(item_parameters, data.operation, "Operation", QIcon(":/icons/model-tree/operation"), [&]
         {
-            NumberDialog dialog(this, "Operation");
-            dialog.addField("Brace height:", "m", data.operation.brace_height);
-            dialog.addField("Draw length:", "m", data.operation.draw_length);
-            dialog.addField("Arrow mass:", "kg", data.operation.mass_arrow);
-            dialog.exec();
+            auto dialog = new NumberDialog(this, "Operation");
+            dialog->addField("Brace height:", "m", data.operation.brace_height);
+            dialog->addField("Draw length:", "m", data.operation.draw_length);
+            dialog->addField("Arrow mass:", "kg", data.operation.mass_arrow);
+            return dialog;
         });
 
         QObject::connect(this, &QTreeWidget::itemActivated, [](QTreeWidgetItem* base_item, int column)
@@ -136,7 +162,7 @@ public:
             auto item = dynamic_cast<TreeItem*>(base_item);
             if(item)
             {
-                item->action();
+                item->show_dialog();
             }
         });
 
