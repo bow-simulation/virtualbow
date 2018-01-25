75176b0b6cf4eef04ce5bb291966ded173adc8bf
Stefan Pfeifer <s-pfeifer@gmx.net>
2018-01-25 21:17:14 +0100

    Adapt model tree

--- source/gui/input/editors/TreeEditor.cpp
+++ source/gui/input/editors/TreeEditor.cpp
@@ -1,1 +1,48 @@
 #include "TreeEditor.hpp"
+#include "TreeItem.hpp"
+#include "gui/input/dialogs/CommentDialog.hpp"
+#include "gui/input/dialogs/ProfileDialog.hpp"
+#include "gui/input/dialogs/WidthDialog.hpp"
+#include "gui/input/dialogs/LayerDialog.hpp"
+#include "gui/input/dialogs/SettingsDialog.hpp"
+#include "gui/input/dialogs/StringDialog.hpp"
+#include "gui/input/dialogs/MassesDialog.hpp"
+#include "gui/input/dialogs/OperationDialog.hpp"
+
+TreeEditor::TreeEditor()
+{
+    new TreeItem<CommentDialog, std::string>(this, data.meta.comments, "Comments", QIcon(":/icons/model-tree/comments"));
+    new TreeItem<SettingsDialog, Settings2>(this, data.settings, "Settings", QIcon(":/icons/model-tree/settings"));
+
+    auto parameters = new QTreeWidgetItem(this, {"Parameters"});
+    parameters->setIcon(0, QIcon(":/icons/model-tree/parameters"));
+
+    new TreeItem<ProfileDialog, Profile2>(parameters, data.profile, "Profile", QIcon(":/icons/model-tree/profile"));
+    new TreeItem<WidthDialog, Series>(parameters, data.width, "Width", QIcon(":/icons/model-tree/width"));
+    new TreeItem<LayerDialog, std::vector<Layer2>>(parameters, data.layers, "Layers", QIcon(":/icons/model-tree/layers"));
+    new TreeItem<StringDialog, String2>(parameters, data.string, "String", QIcon(":/icons/model-tree/string"));
+    new TreeItem<MassesDialog, Masses2>(parameters, data.masses, "Masses", QIcon(":/icons/model-tree/masses"));
+    new TreeItem<OperationDialog, Operation2>(parameters, data.operation, "Operation", QIcon(":/icons/model-tree/operation"));
+
+    QObject::connect(this, &QTreeWidget::itemActivated, [](QTreeWidgetItem* item, int column)
+    {
+        auto action = dynamic_cast<Action*>(item);
+        if(action != nullptr)
+            action->performAction();
+    });
+
+    this->setHeaderLabel("Model Tree");
+    this->expandAll();
+    // this->setItemsExpandable(false);    // Todo: Why is the expansion symbol still visible? (on KDE Desktop at least)
+}
+
+const InputData& TreeEditor::getData() const
+{
+    return data;
+}
+
+
+void TreeEditor::setData(const InputData& data)
+{
+    this->data = data;
+}
--- source/gui/input/editors/TreeEditor.hpp
+++ source/gui/input/editors/TreeEditor.hpp
@@ -1,134 +1,19 @@
 #pragma once
 #include "bow/input/InputData.hpp"
-#include "gui/input/dialogs/CommentDialog.hpp"
-#include "gui/input/dialogs/ProfileDialog.hpp"
-#include "gui/input/dialogs/WidthDialog.hpp"
-#include "gui/input/dialogs/LayerDialog.hpp"
-#include "gui/input/dialogs/SettingsDialog.hpp"
-#include "gui/input/dialogs/StringDialog.hpp"
-#include "gui/input/dialogs/MassesDialog.hpp"
-#include "gui/input/dialogs/OperationDialog.hpp"
 #include <QtWidgets>
-#include <functional>
 
-class TreeItem: public QObject, public QTreeWidgetItem
+class TreeEditor: public QTreeWidget
 {
     Q_OBJECT
 
 public:
-    template<class parent_t>
-    TreeItem(parent_t* parent, const QString& name, const QIcon& icon, std::function<void()> action)
-        : QTreeWidgetItem(parent, {name}),
-          action(action)
-    {
-        this->setIcon(0, icon);
-    }
+    TreeEditor();
+    const InputData& getData() const;
+    void setData(const InputData& data);
 
-    void performAction()
-    {
-        action();
-    }
+signals:
+    void modified();
 
 private:
-    std::function<void()> action;
-};
-
-class TreeEditor: public QTreeWidget
-{
-public:
-    TreeEditor()
-    {
-        new TreeItem(this, "Comments", QIcon(":/icons/model-tree/comments"), [&]{
-            if(comments == nullptr)
-                comments = new CommentDialog(this);
-
-            comments->setData(input.meta.comments);
-            comments->activateWindow();
-            comments->show();
-
-            /*
-            QObject::connect(comments, &LayerDialog::accepted, [&]{
-                input.meta.comments = comments->getData();
-            });
-            */
-        });
-
-        new TreeItem(this, "Settings", QIcon(":/icons/model-tree/settings"), [&]{
-            auto dialog = new SettingsDialog(this);
-            dialog->setData(input.settings);
-            dialog->exec();
-        });
-
-        auto item_parameters = new QTreeWidgetItem(this, {"Parameters"});
-        item_parameters->setIcon(0, QIcon(":/icons/model-tree/parameters"));
-
-        new TreeItem(item_parameters, "Profile", QIcon(":/icons/model-tree/profile"), [&]
-        {
-            auto dialog = new ProfileDialog(this);
-            dialog->setData(input.profile);
-            dialog->exec();
-        });
-
-        new TreeItem(item_parameters, "Width", QIcon(":/icons/model-tree/width"), [&]
-        {
-            auto dialog = new WidthDialog(this);
-            dialog->setData(input.width);
-            dialog->exec();
-        });
-
-        new TreeItem(item_parameters, "Layers", QIcon(":/icons/model-tree/layers"), [&]{
-            if(layers == nullptr)
-                layers = new LayerDialog(this);
-
-            layers->setData(input.layers);
-            layers->activateWindow();
-            layers->show();
-
-            /*
-            QObject::connect(layers, &LayerDialog::accepted, [&]{
-                input.layers = layers->getData();
-            });
-            */
-        });
-
-        new TreeItem(item_parameters, "String", QIcon(":/icons/model-tree/string"), [&]
-        {
-            auto dialog = new StringDialog(this);
-            dialog->setData(input.string);
-            dialog->exec();
-        });
-
-        new TreeItem(item_parameters, "Masses", QIcon(":/icons/model-tree/masses"), [&]
-        {
-            auto dialog = new MassesDialog(this);
-            dialog->setData(input.masses);
-            dialog->exec();
-        });
-
-        new TreeItem(item_parameters, "Operation", QIcon(":/icons/model-tree/operation"), [&]
-        {
-            auto dialog = new OperationDialog(this);
-            dialog->setData(input.operation);
-            dialog->exec();
-        });
-
-        QObject::connect(this, &QTreeWidget::itemActivated, [](QTreeWidgetItem* base_item, int column)
-        {
-            auto item = dynamic_cast<TreeItem*>(base_item);
-            if(item)
-            {
-                item->performAction();
-            }
-        });
-
-        this->setHeaderLabel("Model Tree");
-        this->expandAll();
-        this->setItemsExpandable(false);    // Todo: Why is the expansion symbol still visible? (on KDE Desktop at least)
-    }
-
-private:
-    InputData input;
-
-    LayerDialog* layers = nullptr;
-    CommentDialog* comments = nullptr;
+    InputData data;
 };
