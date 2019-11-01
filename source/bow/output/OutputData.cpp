e5fa591070a7110375589da55ecb8e304bbf6f1f
Stefan Pfeifer <s-pfeifer@gmx.net>
2019-11-02 00:13:15 +0100

    Fix msgpack export on Windows

--- source/bow/output/OutputData.cpp
+++ source/bow/output/OutputData.cpp
@@ -3,7 +3,8 @@
 
 void OutputData::save(const std::string& path) const
 {
+	// https://github.com/nlohmann/json/issues/479#issuecomment-282988808
     std::vector<uint8_t> buffer = json::to_msgpack(*this);
-    std::ofstream file(path);
+    std::ofstream file(path, std::ios::out | std::ios::binary);
     file.write(reinterpret_cast<const char*>(buffer.data()), buffer.size());
 }
