c557f05068da30be1490f91c6d31c7548d761f58
Stefan Pfeifer <s-pfeifer@gmx.net>
2019-11-24 23:41:05 +0100

    Revert dynamic solver to central difference

--- source/fem/DynamicSolver.cpp
+++ source/fem/DynamicSolver.cpp
@@ -1,36 +1,27 @@
 #include "DynamicSolver.hpp"
 
-using namespace boost::numeric::odeint;
-
 DynamicSolver::DynamicSolver(System& system, double dt, double f_sample, const StopFn& stop)
     : system(system),
-      stop_fn(stop),
+      stop(stop),
       dt(dt),
-      n(std::max(std::ceil(1.0/(f_sample*dt)), 1.0)),
-      stepper(make_controlled(1e-6, 1e-4, runge_kutta_cash_karp54<state_type>())),
-      x(2*system.dofs())
+      n(std::max(std::ceil(1.0/(f_sample*dt)), 1.0))
 {
-    // Assign system state to x
-    Eigen::Map<VectorXd> u(&x[0], system.dofs());
-    Eigen::Map<VectorXd> v(&x[system.dofs()], system.dofs());
-    u = system.get_u();
-    v = system.get_v();
+    // Initialise previous displacement
+    u_p2 = system.get_u() - dt*system.get_v() + dt*dt/2.0*system.get_a();
 }
 
 // Estimate timestep based on maximum eigen frequency and a safety factor to account for nonlinearity of the system
 double DynamicSolver::estimate_timestep(const System& system, double factor)
 {
-    Eigen::GeneralizedSelfAdjointEigenSolver<MatrixXd>
-            eigen_solver(system.get_K(), system.get_M().asDiagonal(), Eigen::DecompositionOptions::EigenvaluesOnly);
+    EigenvalueSolver solver(system);
 
-    if(eigen_solver.info() != Eigen::Success)
-        throw std::runtime_error("Failed to compute eigenvalues of the system");
+    auto info = solver.compute();
+    double omega = info.back().omega_0;
+    double zeta = info.back().zeta;
 
-    double omega_max = std::sqrt(eigen_solver.eigenvalues().maxCoeff());
-    if(omega_max == 0.0)
-        throw std::runtime_error("Can't estimate timestep for system with a zero eigenvalue");
+    //https://www.dynasupport.com/tutorial/ls-dyna-users-guide/time-integration
+    return factor*2.0/omega*(std::sqrt(1 + zeta*zeta) - zeta);
 
-    return factor*2.0/omega_max;
 }
 
 bool DynamicSolver::step()
@@ -38,7 +29,7 @@ bool DynamicSolver::step()
     for(unsigned i = 0; i < n; ++i)
     {
         sub_step();
-        if(stop_fn())
+        if(stop())
             return false;
     }
 
@@ -47,27 +38,11 @@ bool DynamicSolver::step()
 
 void DynamicSolver::sub_step()
 {
-    auto f = [&](const state_type& x, state_type& dxdt, const double t)
-    {
-        // Assign state and time to the system
-        Eigen::Map<const VectorXd> u(&x[0], system.dofs());
-        Eigen::Map<const VectorXd> v(&x[system.dofs()], system.dofs());
-        system.set_u(u);
-        system.set_v(v);
-        system.set_t(t);
+    u_p1 = system.get_u();
 
-        // Assign system velocity and acceleration to dxdt
-        Eigen::Map<VectorXd> dxdt_1(&dxdt[0], system.dofs());
-        Eigen::Map<VectorXd> dxdt_2(&dxdt[system.dofs()], system.dofs());
-        dxdt_1 = system.get_v();
-        dxdt_2 = system.get_a();
-    };
+    system.set_u(2.0*system.get_u() - u_p2 + dt*dt*system.get_a());
+    system.set_v((1.5*system.get_u() - 2.0*u_p1 + 0.5*u_p2)/dt);
+    system.set_t(system.get_t() + dt);
 
-    //auto test = boost::numeric::odeint::make_controlled(1e-6, 1e-6, runge_kutta_cash_karp54<state_type>());
-
-    double t = system.get_t();
-    while(stepper.try_step(f, x, t, dt) != controlled_step_result::success)
-    {
-
-    }
+    u_p2 = u_p1;
 }
--- source/fem/DynamicSolver.hpp
+++ source/fem/DynamicSolver.hpp
@@ -1,13 +1,7 @@
 #pragma once
 #include "fem/System.hpp"
 #include "fem/Node.hpp"
-
-#include <boost/numeric/odeint.hpp>
-#include <vector>
-
-using state_type = std::vector<double>;
-using boost::numeric::odeint::runge_kutta_cash_karp54;
-using stepper_type = boost::numeric::odeint::result_of::make_controlled<runge_kutta_cash_karp54<state_type>>::type;
+#include "EigenvalueSolver.hpp"
 
 // Central difference method
 class DynamicSolver
@@ -15,20 +9,19 @@ class DynamicSolver
 public:
     using StopFn = std::function<bool()>;
 
-    DynamicSolver(System& system, double dt, double f_sample, const StopFn& stop_fn);
+    DynamicSolver(System& system, double dt, double f_sample, const StopFn& stop);
     static double estimate_timestep(const System& system, double factor);
     bool step();
 
 private:
     System& system;
-    StopFn stop_fn;
-    double dt;     // Delta for sub steps
+    StopFn stop;
+
+    double dt;     // Substep size
     unsigned n;    // Number of substeps per step
 
-    stepper_type stepper;
-    state_type x;
+    VectorXd u_p2;
+    VectorXd u_p1;
 
     void sub_step();
-    void get_system_state(state_type& state);
-    void set_system_state(const state_type& state);
 };
