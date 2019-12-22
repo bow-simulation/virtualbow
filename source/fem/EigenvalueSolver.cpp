95daf5d5c6dc081be5d0163d862ca4b29d2d1034
Stefan Pfeifer <s-pfeifer@gmx.net>
2019-12-23 00:39:08 +0100

    Change EigenvalueSolver interface

--- source/fem/EigenvalueSolver.cpp
+++ source/fem/EigenvalueSolver.cpp
@@ -1,5 +1,12 @@
 #include "EigenvalueSolver.hpp"
 #include <algorithm>
+#include <boost/optional.hpp>
+
+ModeInfo::ModeInfo(std::complex<double> lambda)
+{
+    omega = std::hypot(lambda.real(), lambda.imag());
+    zeta = lambda.real()/omega;
+}
 
 EigenvalueSolver::EigenvalueSolver(const System& system)
     : system(system)
@@ -7,48 +14,67 @@ EigenvalueSolver::EigenvalueSolver(const System& system)
 
 }
 
-std::vector<EigenvalueSolver::ModeInfo> EigenvalueSolver::compute()
+auto EigenvalueSolver::compute_eigenvalues()
 {
     int n = system.dofs();
-
     A.conservativeResize(2*n, 2*n);
-    A << MatrixXd::Zero(n, n), MatrixXd::Identity(n, n),
-         -system.get_K(), -system.get_D();
-
     B.conservativeResize(2*n, 2*n);
-    B << MatrixXd::Identity(n, n), MatrixXd::Zero(n, n),
-         MatrixXd::Zero(n, n), system.get_M().asDiagonal().toDenseMatrix();
 
-    eigen_solver.compute(A, B, Eigen::DecompositionOptions::EigenvaluesOnly);
-    if(eigen_solver.info() != Eigen::Success)
-        throw std::runtime_error("Failed to compute eigenvalues of the system. Solver info: " + std::to_string(eigen_solver.info()));
+    A << MatrixXd::Zero(n, n), system.get_K(),
+         system.get_K(), system.get_D();
 
-    std::vector<ModeInfo> results;
-    auto evs = eigen_solver.eigenvalues();
-    for(int i = 0; i < evs.size(); ++i)
+    B << system.get_K(), MatrixXd::Zero(n, n),
+         MatrixXd::Zero(n, n), -system.get_M().asDiagonal().toDenseMatrix();
+
+    solver.compute(A, B, Eigen::DecompositionOptions::EigenvaluesOnly);
+    if(solver.info() != Eigen::Success)
+        throw std::runtime_error("Failed to compute eigenvalues of the system. Solver info: " + std::to_string(solver.info()));
+
+    return solver.eigenvalues();
+}
+
+// Todo: Implement with filter + minimum
+ModeInfo EigenvalueSolver::compute_minimum_frequency()
+{
+    boost::optional<ModeInfo> result = boost::none;
+    auto eigenvalues = compute_eigenvalues();
+    for(int i = 0; i < eigenvalues.size(); ++i)
     {
-        if(std::abs(evs(i).imag()) < 1e-12)    // Magic number
+        if(eigenvalues[i].imag() > 0.0)
         {
-            // Real eigenvalue: Skip
-            continue;
-        }
-        else
-        {
-            // Complex eigenvalue
-            double omega_0 = std::hypot(evs[i].real(), evs[i].imag());
-            double zeta = -evs[i].real()/omega_0;
-            results.push_back({omega_0, zeta});
-
-            // Skip next conjugated eigenvalue
-            ++i;
+            ModeInfo temp(eigenvalues[i]);
+            if(!result || temp.omega < result->omega)
+            {
+                result = temp;
+            }
         }
     }
 
-    // Sort by undamped eigenfrequency
-    std::sort(results.begin(), results.end(), [](const ModeInfo& a, const ModeInfo& b) {
-        return a.omega_0 < b.omega_0;
-    });
+    if(!result)
+        throw std::runtime_error("Failed to find eigenvalue with lowest natural frequency");
 
-    return results;
+    return *result;
 }
 
+// Todo: Implement with filter + maximum
+ModeInfo EigenvalueSolver::compute_maximum_frequency()
+{
+    boost::optional<ModeInfo> result = boost::none;
+    auto eigenvalues = compute_eigenvalues();
+    for(int i = 0; i < eigenvalues.size(); ++i)
+    {
+        if(eigenvalues[i].imag() > 0.0)
+        {
+            ModeInfo temp(eigenvalues[i]);
+            if(!result || temp.omega > result->omega)
+            {
+                result = temp;
+            }
+        }
+    }
+
+    if(!result)
+        throw std::runtime_error("Failed to find eigenvalue with highest natural frequency");
+
+    return *result;
+}
