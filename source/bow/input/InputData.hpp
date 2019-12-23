9c5ec0a25f78e5aa071c6307c15c5c3970513814
Stefan Pfeifer <s-pfeifer@gmx.net>
2019-12-23 02:28:25 +0100

    Change default settings

--- source/bow/input/InputData.hpp
+++ source/bow/input/InputData.hpp
@@ -15,7 +15,7 @@ struct InputData
 {
     Meta meta;
     Settings settings;
-    Series profile = {{0.1, 0.8}, {0.0, 0.5}};
+    Series profile = {{0.0, 0.8}, {0.0, 0.0}};
     Series width = {{0.0, 1.0}, {0.06, 0.01}};
     Layers layers = {Layer()};
     String string;
