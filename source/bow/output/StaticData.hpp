07cc8e0ca7a6db21af705854d503fb1293d3fa52
Stefan Pfeifer <s-pfeifer@gmx.net>
2020-03-20 18:38:23 +0000

    Add energies at arrow departure to dynamic output

--- source/bow/output/StaticData.hpp
+++ source/bow/output/StaticData.hpp
@@ -7,12 +7,11 @@ using nlohmann::json;
 struct StaticData
 {
     BowStates states;
-    double limb_mass = 0.0;
-    double string_mass = 0.0;
-    double string_length = 0.0;
+
     double final_draw_force = 0.0;
     double drawing_work = 0.0;
     double storage_ratio = 0.0;
+
     double max_string_force = 0.0;
     double max_strand_force = 0.0;
     double max_grip_force = 0.0;
@@ -24,13 +23,9 @@ struct StaticData
 static void to_json(json& obj, const StaticData& val)
 {
     obj["states"] = val.states;
-    obj["limb_mass"] = val.limb_mass;
-    obj["string_mass"] = val.string_mass;
-    obj["string_length"] = val.string_length;
     obj["final_draw_force"] = val.final_draw_force;
     obj["drawing_work"] = val.drawing_work;
     obj["storage_ratio"] = val.storage_ratio;
-
     obj["max_string_force"] = val.max_string_force;
     obj["max_strand_force"] = val.max_strand_force;
     obj["max_grip_force"] = val.max_grip_force;
