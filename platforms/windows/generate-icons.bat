eceb9ec4fe21f605adbce97b75204f829729856f
Stefan Pfeifer <s-pfeifer@gmx.net>
2020-04-14 16:33:02 +0100

    Modify icons, reset Windows installer icon to default

---
+++ platforms/windows/generate-icons.bat
@@ -0,0 +1,3 @@
+magick convert -background none ../../resources/icons/filetype-bow.svg -define icon:auto-resize filetype-bow.ico
+magick convert -background none ../../resources/icons/filetype-vbr.svg -define icon:auto-resize filetype-vbr.ico
+magick convert -background none ../../resources/icons/logo.svg -define icon:auto-resize logo.ico
