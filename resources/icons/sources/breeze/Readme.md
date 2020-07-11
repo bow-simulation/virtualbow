10ffa15eab7ae6d39c3846ac4b3a1d82dbabf0d9
Stefan Pfeifer <s-pfeifer@gmx.net>
2020-07-12 01:28:04 +0200

    More visual improvements

---
+++ resources/icons/sources/breeze/Readme.md
@@ -0,0 +1,3 @@
+Source: Breeze icon set from KDE
+Version: v5.72.0 (15d52da)
+Link: https://github.com/KDE/breeze-icons
--- source/post/MainWindow.cpp
+++ source/post/MainWindow.cpp
@@ -41,6 +41,7 @@ MainWindow::MainWindow()
     // Main window
     this->setWindowIcon(QIcon(":/icons/logo.png"));
     this->setStyleSheet("QMainWindow { background-image:url(:/icons/background.png); background-position: center; background-repeat: no-repeat; }");
+    this->menuBar()->setAutoFillBackground(true);
     this->resize(INITIAL_SIZE);
 
     // Load geometry and state
