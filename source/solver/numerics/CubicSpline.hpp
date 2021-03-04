4381bccfd2a08621d3799de27f725b60fb0971aa
Stefan Pfeifer <s-pfeifer@gmx.net>
2021-03-04 02:00:11 +0100

    Revert to previous monotonic spline implementation

--- source/solver/numerics/CubicSpline.hpp
+++ source/solver/numerics/CubicSpline.hpp
@@ -4,19 +4,14 @@
 #include <cstddef>
 
 // Cubic spline that preserves monotonicity of the input data.
-// Implementation adapted from [1]
-//
-// [1] George Wolfberg, Itzik Alfx: An energy-minimization framework for monotonic cubic spline interpolation
-//     Journal of Computational and Applied Mathematics, Vol. 143, No. 2, pages 145-188, 2002.
-//     https://www.sciencedirect.com/science/article/pii/S0377042701005064
-
+// Implementation adapted from https://en.wikipedia.org/wiki/Monotone_cubic_interpolation
+// Throws std::invalid_argument on invalid input
 class CubicSpline
 {
 public:
     CubicSpline(const MatrixXd& input);
-
-    double operator()(double arg) const;
-    double operator()(double arg, double default_value) const;
+    double operator()(double x) const;
+    double operator()(double x, double y_default) const;
 
     double arg_min() const;
     double arg_max() const;
@@ -24,7 +19,9 @@ public:
 private:
     mutable size_t index = 0;    // Interval index of last evaluation
 
-    std::vector<double> x;
-    std::vector<double> y;
-    std::vector<double> m;
+    std::vector<double> xs;
+    std::vector<double> ys;
+    std::vector<double> c1s;
+    std::vector<double> c2s;
+    std::vector<double> c3s;
 };
