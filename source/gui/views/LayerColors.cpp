4703ba771acca35bb46512f24eeee4e926d02e7c
Stefan Pfeifer <s-pfeifer@gmx.net>
2021-03-05 02:51:39 +0100

    Some adjusting of the layer colors

--- source/gui/views/LayerColors.cpp
+++ source/gui/views/LayerColors.cpp
@@ -18,9 +18,9 @@ QColor getLayerColor(const LayerProperties& layer)
 QColor getLayerColor(double rho, double E)
 {
     // Random distributions for HSV color components
-    std::uniform_int_distribution<int> h(15, 60);
-    std::uniform_int_distribution<int> s(175, 255);
-    std::uniform_int_distribution<int> v(225, 255);
+    std::uniform_int_distribution<int> h(0, 90);
+    std::uniform_int_distribution<int> s(150, 255);
+    std::uniform_int_distribution<int> v(200, 255);
 
     std::mt19937 rng((unsigned) (rho*E));    // Seed randum number generator with material properties
     return QColor::fromHsv(h(rng), s(rng), v(rng));    // Generate random color from distributions
