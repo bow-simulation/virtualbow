ce0b64dd74bab6a6443b0457fe6a2946d00c0a2d
Stefan Pfeifer <s-pfeifer@gmx.net>
2021-03-21 01:45:06 +0100

    Revert back to instantiating model dialogs only when needed

--- source/gui/dialogs/CommentDialog.hpp
+++ source/gui/dialogs/CommentDialog.hpp
@@ -9,7 +9,7 @@ class CommentDialog: public PersistentDialog
     Q_OBJECT
 
 public:
-    CommentDialog(QWidget* parent);
+    CommentDialog(QWidget* parent, const UnitSystem& units);
 
     std::string getData() const;
     void setData(const std::string& text);
--- source/gui/editors/TreeEditor.cpp
+++ source/gui/editors/TreeEditor.cpp
@@ -1,85 +1,45 @@
 #include "TreeEditor.hpp"
+#include "TreeItem.hpp"
+#include "gui/dialogs/CommentDialog.hpp"
+#include "gui/dialogs/ProfileDialog.hpp"
+#include "gui/dialogs/WidthDialog.hpp"
+#include "gui/dialogs/LayerDialog.hpp"
+#include "gui/dialogs/SettingsDialog.hpp"
+#include "gui/dialogs/StringDialog.hpp"
+#include "gui/dialogs/MassesDialog.hpp"
+#include "gui/dialogs/DampingDialog.hpp"
+#include "gui/dialogs/DimensionsDialog.hpp"
 
-DialogTreeItem::DialogTreeItem(QTreeWidget* parent, const QString& name, const QIcon& icon, QDialog* dialog)
-    : QTreeWidgetItem(parent, {name}),
-      dialog(dialog) {
-    this->setIcon(0, icon);
-}
+TreeEditor::TreeEditor(const UnitSystem& units)
+{
+    new TreeItem<CommentDialog, std::string>(this, units, data.comment, "Comments", QIcon(":/icons/model-comments.svg"));
+    new TreeItem<SettingsDialog, Settings>(this, units, data.settings, "Settings", QIcon(":/icons/model-settings.svg"));
+    new TreeItem<DimensionsDialog, Dimensions>(this, units, data.dimensions, "Dimensions", QIcon(":/icons/model-dimensions.svg"));
+    new TreeItem<ProfileDialog, MatrixXd>(this, units, data.profile, "Profile", QIcon(":/icons/model-profile.svg"));
+    new TreeItem<WidthDialog, MatrixXd>(this, units, data.width, "Width", QIcon(":/icons/model-width.svg"));
+    new TreeItem<LayerDialog, std::vector<Layer>>(this, units, data.layers, "Layers", QIcon(":/icons/model-layers.svg"));
+    new TreeItem<StringDialog, String>(this, units, data.string, "String", QIcon(":/icons/model-string.svg"));
+    new TreeItem<MassesDialog, Masses>(this, units, data.masses, "Masses", QIcon(":/icons/model-masses.svg"));
+    new TreeItem<DampingDialog, Damping>(this, units, data.damping, "Damping", QIcon(":/icons/model-damping.svg"));
 
-void DialogTreeItem::showDialog() {
-    dialog->show();
-}
-
-TreeEditor::TreeEditor(const UnitSystem& units) {
-    dialog_comments = new CommentDialog(this);
-    QObject::connect(dialog_comments, &CommentDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Comments", QIcon(":/icons/model-comments.svg"), dialog_comments);
-
-    dialog_settings = new SettingsDialog(this, units);
-    QObject::connect(dialog_settings, &SettingsDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Settings", QIcon(":/icons/model-settings.svg"), dialog_settings);
-
-    dialog_dimensions = new DimensionsDialog(this, units);
-    QObject::connect(dialog_dimensions, &DimensionsDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Dimensions", QIcon(":/icons/model-dimensions.svg"), dialog_dimensions);
-
-    dialog_profile = new ProfileDialog(this, units);
-    QObject::connect(dialog_profile, &ProfileDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Profile", QIcon(":/icons/model-profile.svg"), dialog_profile);
-
-    dialog_width = new WidthDialog(this, units);
-    QObject::connect(dialog_width, &WidthDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Width", QIcon(":/icons/model-width.svg"), dialog_width);
-
-    dialog_layers = new LayerDialog(this, units);
-    QObject::connect(dialog_layers, &LayerDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Layers", QIcon(":/icons/model-layers.svg"), dialog_layers);
-
-    dialog_string = new StringDialog(this, units);
-    QObject::connect(dialog_string, &StringDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "String", QIcon(":/icons/model-string.svg"), dialog_string);
-
-    dialog_masses = new MassesDialog(this, units);
-    QObject::connect(dialog_masses, &MassesDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Masses", QIcon(":/icons/model-masses.svg"), dialog_masses);
-
-    dialog_damping = new DampingDialog(this, units);
-    QObject::connect(dialog_damping, &DampingDialog::modified, this, &TreeEditor::modified);
-    new DialogTreeItem(this, "Damping", QIcon(":/icons/model-damping.svg"), dialog_damping);
-
-    QObject::connect(this, &QTreeWidget::itemActivated, [=](QTreeWidgetItem* item, int column) {
-        auto dialog_item = static_cast<DialogTreeItem*>(item);
-        dialog_item->showDialog();
+    QObject::connect(this, &QTreeWidget::itemActivated, [](QTreeWidgetItem* item, int column) {
+        auto action = dynamic_cast<Action*>(item);
+        if(action != nullptr) {
+            action->performAction();
+        }
     });
 
     this->setHeaderLabel("Model");
     this->expandAll();
-    this->setItemsExpandable(false);
+    this->setItemsExpandable(false);    // Todo: Why is the expansion symbol still visible? (on KDE Desktop at least)
 }
 
-InputData TreeEditor::getData() const {
-    InputData data;
-    data.comment = dialog_comments->getData();
-    data.settings = dialog_settings->getData();
-    data.dimensions = dialog_dimensions->getData();
-    data.profile = dialog_profile->getData();
-    data.width = dialog_width->getData();
-    data.layers = dialog_layers->getData();
-    data.string = dialog_string->getData();
-    data.masses = dialog_masses->getData();
-    data.damping = dialog_damping->getData();
-
+const InputData& TreeEditor::getData() const
+{
     return data;
 }
 
-void TreeEditor::setData(const InputData& data) {
-    dialog_comments->setData(data.comment);
-    dialog_settings->setData(data.settings);
-    dialog_dimensions->setData(data.dimensions);
-    dialog_profile->setData(data.profile);
-    dialog_width->setData(data.width);
-    dialog_layers->setData(data.layers);
-    dialog_string->setData(data.string);
-    dialog_masses->setData(data.masses);
-    dialog_damping->setData(data.damping);
+void TreeEditor::setData(const InputData& data)
+{
+    this->data = data;
 }
