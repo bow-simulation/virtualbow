6a305b0c7547d9c375639b56e31fc89e888a99c7
Stefan Pfeifer <s-pfeifer@gmx.net>
2021-08-29 17:17:52 +0200

    More profile curve UI experimentation

--- source/gui/editors/ProfileEditor.hpp
+++ source/gui/editors/ProfileEditor.hpp
@@ -4,10 +4,50 @@
 #include "gui/units/UnitGroup.hpp"
 #include "TableView.hpp"
 #include "TableModel.hpp"
+#include "solver/model/ProfileCurve.hpp"
+
+class SpinBoxDelegate: public QStyledItemDelegate {
+    Q_OBJECT
+
+public:
+    SpinBoxDelegate(QObject* parent = nullptr);
+
+    QWidget *createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+    void setEditorData(QWidget* editor, const QModelIndex& index) const override;
+    void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const override;
+    void updateEditorGeometry(QWidget* editor, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+};
+
+class ComboBoxDelegate: public QStyledItemDelegate {
+    Q_OBJECT
+
+public:
+    ComboBoxDelegate(QObject* parent = nullptr);
+
+    QWidget *createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+    void setEditorData(QWidget* editor, const QModelIndex& index) const override;
+    void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const override;
+    void updateEditorGeometry(QWidget* editor, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+};
+
 
 class ProfileTreeHeader: public QHeaderView {
 public:
     ProfileTreeHeader(QWidget* parent, const QList<QToolButton*>& buttons);
+};
+
+class ProfileTreeItem: public QTreeWidgetItem {
+public:
+    ProfileTreeItem(const SegmentInput& segment);
+    QWidget* getEditor();
+
+private:
+    SegmentInput segment;
+    QWidget* editor;
+
+    QIcon segmentIcon(SegmentType type) const;
+    QString segmentText(SegmentType type) const;
+    QWidget* segmentEditor(SegmentType type) const;
 };
 
 class ProfileEditor: public QWidget
--- source/gui/editors/ProfileTableModel.hpp
+++ source/gui/editors/ProfileTableModel.hpp
@@ -14,8 +14,14 @@ public:
     QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
+    // Editing
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+
 private:
     std::vector<Constraint> constraints;
 
     QString constraintText(ConstraintType type) const;
+
+    bool checkIndex(const QModelIndex &index) const;
 };
