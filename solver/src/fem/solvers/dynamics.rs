dd69cb6ff4e31c160bf58d458d01cc49a923a6d1
Stefan Pfeifer <s-pfeifer@gmx.net>
2024-09-10 23:56:32 +0200

    Revert dynamic solver back to classic newmark method

--- solver/src/fem/solvers/dynamics.rs
+++ solver/src/fem/solvers/dynamics.rs
@@ -3,7 +3,6 @@ use crate::fem::system::system::{System, DynamicEval};
 
 #[derive(Copy, Clone)]
 pub struct Settings {
-    pub method: MethodParameters,
     pub timestep: f64,          // Time step
     pub epsilon_rel: f64,       // Relative tolerance
     pub epsilon_abs: f64,       // Absolute tolerance
@@ -14,47 +13,11 @@ pub struct Settings {
 impl Default for Settings {
     fn default() -> Self {
         Self {
-            method: MethodParameters::newmark(),
             timestep: 1e-6,
             epsilon_rel: 1e-08,
             epsilon_abs: 1e-10,
             max_iterations: 50,
             max_stagnation: 10
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
-pub struct MethodParameters {
-    alpha_m: f64,
-    alpha_f: f64,
-    beta: f64,
-    gamma: f64,
-}
-
-impl MethodParameters {
-    // Newmark with parameters for average constant acceleration (unconditionally stable in the linear case)
-    pub fn newmark() -> Self {
-        Self {
-            alpha_m: 0.0,
-            alpha_f: 0.0,
-            beta: 0.25,
-            gamma: 0.50,
-        }
-    }
-
-    // Generalized alpha method with spectral radium rho_inf € [0, 1] at infinity
-    pub fn gen_alpha(rho_inf: f64) -> Self {
-        assert!(rho_inf >= 0.0 && rho_inf <= 1.0);
-
-        let alpha_m = (2.0*rho_inf - 1.0)/(rho_inf + 1.0);
-        let alpha_f = rho_inf/(rho_inf + 1.0);
-
-        Self {
-            beta: 0.25*(1.0 - alpha_m + alpha_f).powi(2),
-            gamma: 0.5 - alpha_m + alpha_f,
-            alpha_m,
-            alpha_f,
         }
     }
 }
@@ -75,12 +38,9 @@ impl<'a> DynamicSolver<'a> {
     pub fn solve<F>(&mut self, callback: &mut F)
         where F: FnMut(&System, &DynamicEval) -> bool
     {
-        // Commonly used settings
-        let alpha_m = self.settings.method.alpha_m;
-        let alpha_f = self.settings.method.alpha_f;
-        let beta  = self.settings.method.beta;
-        let gamma = self.settings.method.gamma;
-        let dt = self.settings.timestep;
+        // Constant average acceleration (unconditionally stable for linear systems)
+        let beta  = 0.25;
+        let gamma = 0.50;
 
         // Dynamic system state
         let mut eval = self.system.create_dynamic_eval();
@@ -95,16 +55,15 @@ impl<'a> DynamicSolver<'a> {
             return;
         }
 
+        let dt = self.settings.timestep;
+
         loop {
             // Current displacements, velocities and accelerations
             let u_current = self.system.get_displacements().clone();
             let v_current = self.system.get_velocities().clone();
             let a_current = eval.get_accelerations().clone();
-            let p_current = eval.get_external_forces().clone();
-            let q_current = eval.get_internal_forces().clone();
 
-            // Next displacements, velocities and accelerations to be iterated on
-            let mut a_next = a_current.clone();  // Initial value is last value
+            let mut a_next = a_current.clone();  // DVector::zeros(n);
             let mut v_next = &v_current + dt*(1.0 - gamma)*&a_current + dt*gamma*&a_next;
             let mut u_next = &u_current + dt*&v_current + dt*dt*((0.5 - beta)*&a_current + beta*&a_next);
 
@@ -115,15 +74,8 @@ impl<'a> DynamicSolver<'a> {
                 self.system.set_velocities(&v_next);
                 self.system.eval_dynamics(&mut eval);
 
-                let p_next = eval.get_external_forces();
-                let q_next = eval.get_internal_forces();
-
-                let a_alpha = (1.0 - alpha_m)*&a_next + alpha_m*&a_current;
-                let p_alpha = (1.0 - alpha_f)*p_next + alpha_f*&p_current;
-                let q_alpha = (1.0 - alpha_f)*q_next + alpha_f*&q_current;
-
-                let r: DVector<f64> = p_alpha - q_alpha - eval.get_mass_matrix().component_mul(&a_alpha);
-                let drda: DMatrix<f64> = (1.0 - alpha_m)*DMatrix::<f64>::from_diagonal(eval.get_mass_matrix()) + (1.0 - alpha_f)*(dt*gamma*eval.get_damping_matrix() + dt*dt*beta*eval.get_stiffness_matrix());
+                let r: DVector<f64> = eval.get_external_forces() - eval.get_internal_forces() - eval.get_mass_matrix().component_mul(&a_next);
+                let drda: DMatrix<f64> = DMatrix::<f64>::from_diagonal(eval.get_mass_matrix()) + dt*gamma*eval.get_damping_matrix() + dt*dt*beta*eval.get_stiffness_matrix();
 
                 let decomposition = drda.lu();
                 let delta_a = decomposition.solve(&r).unwrap();
