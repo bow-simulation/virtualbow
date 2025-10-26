f5b3307ad251be1ba41d87bf774a91ff39aca570
Stefan Pfeifer <s-pfeifer@gmx.net>
2025-10-26 16:13:04 +0100

    Fix color button on Windows and improve appearance

--- gui/source/pre/views/primitive/ColorView.cpp
+++ gui/source/pre/views/primitive/ColorView.cpp
@@ -12,7 +12,7 @@ ColorView::ColorView(QAbstractItemModel* model, QPersistentModelIndex index, con
 
     // Open solor dialog on click
     QObject::connect(this, &QPushButton::clicked, this, [=, this]{
-        auto dialog(new QColorDialog(this));
+        auto dialog(new QColorDialog());
         dialog->setOption(QColorDialog::DontUseNativeDialog);
         dialog->setWindowModality(Qt::ApplicationModal);
         dialog->setWindowTitle("Color");
@@ -21,33 +21,33 @@ ColorView::ColorView(QAbstractItemModel* model, QPersistentModelIndex index, con
         // Update model and button on color changes
         QObject::connect(dialog, &QColorDialog::currentColorChanged, this, [=, this] {
             model->setData(index, dialog->currentColor());
-            setColor(initial);
+            setColor(dialog->currentColor());
         });
 
         // When the dialog is closed, either keep the selected color or reset to initial color
         if(dialog->exec() == QDialog::Accepted) {
             setColor(dialog->currentColor());
-        } else {
+        }
+        else {
             model->setData(index, initial);
+            setColor(initial);
         }
     });
 }
 
 void ColorView::setColor(const QColor& color) {
-    // Set button text to show color code
-    setText(color.name());
+    // Set button text to show color in rgb format
+    setText(QString("RGB %1, %2, %3").arg(color.red()).arg(color.green()).arg(color.blue()));
 
-    // Set background color of the button to the given color
-    QPalette pal = palette();
-    pal.setColor(QPalette::Button, color);
+    // Pick text color depending on brightness of the background color
+    // https://stackoverflow.com/a/1855903
+    double luminance = 0.299*color.redF() + 0.587*color.greenF() + 0.114*color.blueF();
+    QColor textColor = (luminance > 0.5) ? Qt::black : Qt::white;
 
-    // Set the text color depending on brightness of the background
-    if(color.lightness() < 200) {
-        pal.setColor(QPalette::ButtonText, Qt::white);
-    }
-    else {
-        pal.setColor(QPalette::ButtonText, Qt::darkGray);
-    }
+    // Generate style string (using QPalette didn't change the background color on Windows)
+    QString style = QString("background-color: %1; color: %2;")
+        .arg(color.name())
+        .arg(textColor.name());
 
-    setPalette(pal);
+    setStyleSheet(style);
 }
